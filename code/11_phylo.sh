9baf42a462c2bf25d29f65306242005ebcf80848
mhelmkampf <martin.helmkampf@leibniz-zmt.de>
2025-07-11 01:12:07 +0200

    Jul 11, 2025

--- code/11_phylo.sh
+++ code/11_phylo.sh
@@ -39,6 +39,7 @@ cd ~/work/phylo
 # - identify the outgroup
 
 
+
 ### ============================================================================
 ### Exercise 2: Infer distance-based phylogenetic tree
 
@@ -50,6 +51,17 @@ cp ~/meg25/data/phylo/*.fas .
 cat Rhyncho_coi_databases.fas Rhyncho_coi_samples.fas > Rhyncho_coi_combined.fas
 
 
+### Print combined Fasta file to screen
+#>
+
+
+### Align sequences using MAFFT (server version)
+### https://mafft.cbrc.jp/alignment/server/
+
+# - Copy and paste combined Fasta file into input box
+# - Select Output order: Same as input, Strategy: Auto and submit
+
+
 
 ### ============================================================================
 ### Exercise 3: Infer phylogenetic tree using maximum likelihood
@@ -57,4 +69,35 @@ cat Rhyncho_coi_databases.fas Rhyncho_coi_samples.fas > Rhyncho_coi_combined.fas
 ### Align sequences using MAFFT
 module load MAFFT
 
+mafft Rhyncho_coi_combined.fas > Rhyncho_coi_combined.aln
 
+
+### Perform maximum likelihood + bootstrap analysis with RAxML-NG
+### (https://github.com/amkozlov/raxml-ng/wiki)
+module load IQ-TREE
+
+raxml-ng \
+    --all \
+    --msa Rhyncho_coi_combined.aln \
+    --model GTR+G \
+    --bs-trees 1000
+
+
+### Identify and print log file to screen
+#>
+
+
+### Print best ML tree with bootstrap support values to screen (Newick format)
+#>
+
+
+### Visualize tree on the Interactive Tree of Life website
+### https://itol.embl.de
+
+# - Upload | copy and paste tree in Newick format into "Tree text" box
+# - Root tree: click on Rhina ancylostoma branch / tip | Tree structure | Re-root the tree here
+# - Increase branch width: Branch option | Line style
+# - Display support values: Advanced | Bootstrap / metadata > Display | Symbol or Text (range 50-100)
+
+
+### How do the distance and maximum likelihood tree differ?
