e728e82ea6183a58568031e28b81f377a1ba5244
mhelmkampf <martin.helmkampf@leibniz-zmt.de>
2025-07-11 00:33:43 +0200

    Jul 11, 2025

--- code/11_phylo.sh
+++ code/11_phylo.sh
@@ -47,68 +47,14 @@ cp ~/meg25/data/phylo/*.fas .
 
 
 ### Combine to single file
-cat 
-
-
-
-
-### ============================================================================
-### Exercise 2: Match sequence to Barcode of Life Data Systems (BOLD)
-
-
-### Visit https://v4.boldsystems.org/index.php
-### Note: https://boldsystems.org (version 5) does not have full functionality yet
-
-
-### Select "Identification v4" | Animal Identification (COI) | Species Level Barcode Records (default)
-
-
-### Paste your consensus sequence and submit
-
-
-### Explore the results:
-### - What species has been identified?
-### - Are there multiple species in the top 20? What is the similarity distribution?
-### – Is it a plausible match considering the samples were obtained from seafood?
-
-
-### Optional: Learn more about your species at https://fishbase.de
-
-
-### Evaluate the reliability of your result by selecting its "Bin page"
-### - What is the maximum p-distance within the species?
-### – How does the distance distribution compare to the nearest neighbor? Is there a "barcode gap"?
-### - How confident are you overall in your id?
-
-
-### Optional: Identify barcode with BLAST: https://blast.ncbi.nlm.nih.gov/Blast.cgi
+cat Rhyncho_coi_databases.fas Rhyncho_coi_samples.fas > Rhyncho_coi_combined.fas
 
 
 
 ### ============================================================================
-### Solutions:
+### Exercise 3: Infer phylogenetic tree using maximum likelihood
 
-### Create consensus
-merger \
-  -asequence trim_10F.fas \
-  -bsequence trim_10R-rc.fas \
-  -outfile consensus_10.txt \
-  -outseq consensus_10.fas
+### Align sequences using MAFFT
+module load MAFFT
 
 
-### All consensus sequences can be found in data/barcode/co1.fas
-
-
-### Species IDs, BIN, barcode gap
-
-# 10: Oncorhynchus keta (Chum salmon / Keta-Lachs), BOLD:AAA3872, large barcode gap
-# 11: Gadus chalcogrammus (Alaska pollock / Paz. Pollack), BOLD:AAA3069, small barcode gap
-# 14: Platichthys flesus (European flounder / Flunder) or Pleuronectes sp., cannot be identified to species
-# 15: Gadus chalcogrammus (Alaska pollock / Paz. Pollack), BOLD:AAA3069, small barcode gap
-# 19: Gadus chalcogrammus (Alaska pollock / Paz. Pollack), BOLD:AAA3069, small barcode gap
-# 20: Platichthys flesus (European flounder / Flunder) or Pleuronectes sp., cannot be identified to species
-# 22: Gadus morhua (Atlantic cod / Kabeljau) or Gadus sp., small barcode gap / cannot be identified to species
-# 26: Gadus morhua (Atlantic cod / Kabeljau) or Gadus sp. small barcode gap /cannot be identified to species
-# 27: Pollachius virens (Pollock / Köhler), BOLD:AAB2980, large barcode gap
-# 28: Oncorhynchus keta (Chum salmon / Keta-Lachs), BOLD:AAA3872, large barcode gap
-# 35: Coregonus sp. (Cisco), cannot be identified to species
